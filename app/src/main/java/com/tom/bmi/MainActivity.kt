6f071f21db31a8a2bc9783784dac4d157d6f86ef
Hank Tom <hanktom@gmail.com>
2018-02-23 20:24:45 +0800

    initial commit

---
+++ app/src/main/java/com/tom/bmi/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.tom.bmi
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
